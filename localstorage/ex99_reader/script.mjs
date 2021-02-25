de4ee1b7568a8469bb29050e821954354a6fecf9
Rich Boakes <rjb@port.ac.uk>
2021-02-25 18:07:28 +0000

    Add comments. Fix small load bug.

--- localstorage/ex99_reader/script.mjs
+++ localstorage/ex99_reader/script.mjs
@@ -2,6 +2,12 @@ const DEFAULTS = {};
 const OPTIONS = [];
 const el = {};
 
+/* For any change to the page's input fields the configuration
+ * should be saved.  This is achievd by constructing a new
+ * object based on input field values, saving it, and then
+ * updating CSS variables so the change is also reflected in
+ * the page.
+ */
 function handleConfigChange() {
 
   const config = {};
@@ -13,11 +19,17 @@ function handleConfigChange() {
   updateCSSVariables(config);
 }
 
+/* Store a configuration object in localStorage
+ */
 function saveConfig(config) {
   const configAsString = JSON.stringify(config);
   localStorage.setItem("readerConfig", configAsString);
 }
 
+
+/* Load the config object from localStorage.  If no object
+ * can be loaded (e.g. first run) an empty object is returned.
+ */
 function loadConfig() {
   const configAsString = localStorage.getItem("readerConfig");
   try {
@@ -28,12 +40,16 @@ function loadConfig() {
   }
 }
 
+/* Given a config object, update the document's custom
+ * CSS properties with values from that object.
+ */
 function updateCSSVariables(config) {
   const rootStyle = document.documentElement.style;
   for (const opt of OPTIONS) {
     rootStyle.setProperty('--'+opt, config[opt]+"em" );
   }
 }
+
 function init() {
 
   /**
@@ -54,14 +70,15 @@ function init() {
     el[opt].addEventListener('input', handleConfigChange);
   }
 
-  /* Load previously stired config and modify input fields
+  /* Load previously stored config and modify input fields
    * to reflect those values.
    */
   const config = loadConfig();
   for (const opt of OPTIONS) {
-    el[opt].value = config[opt]+"em";
+    el[opt].value = config[opt];
   }
 
+  /* Use the config options in the page */
   updateCSSVariables(config);
 
 }
