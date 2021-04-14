a590f155890b43d38030d0b9f170963ee8ed41e2
Rich Boakes <rjb@port.ac.uk>
2021-04-14 17:08:39 +0100

    improved autocue to use transform which can be reversed (unlike keyframe animation).

--- localstorage/ex99_drag/autocue/autocue.mjs
+++ localstorage/ex99_drag/autocue/autocue.mjs
@@ -1,37 +1,49 @@
 // Scroll certain amounts from current position
-
-let scrolling;
+const el = {};
+let scrolling = false;
+let offset = 0;
+let direction = 1;
 
 function startScroll(dir) {
-  const main = document.querySelector("main");
-  main.style["animation-fill-mode"] = dir;
-  main.style.animationDirection="reverse";
-
-}
-
-function resetScroll() {
-}
-
-
-function toggleAnimation() {
-  const main = document.querySelector("main");
-  const state = main.style["animation-play-state"];
-  if (state === "running") {
-    main.style["animation-play-state"] = "paused";
+  if (scrolling) {
+    direction += dir;
   } else {
-    main.style["animation-play-state"] = "running";
+    direction = dir;
+    toggleAnimation();
   }
 }
 
+function resetScroll() {
+  offset = 0;
+  scrolling = false;
+  step();
+}
+
+function toggleAnimation() {
+  scrolling = !scrolling;
+  if (scrolling) step();
+}
+
+function step() {
+  offset += direction;
+
+  if (offset < 0) offset = 0;
+  if (offset > el.main.clientHeight) offset = el.main.clientHeight;
+
+  document.body.style.setProperty("--offset", (0-offset)+"px");
+  if (scrolling) window.requestAnimationFrame(step);
+}
+
+
 function controlScroll(e) {
-  console.log(e.key);
   if (e.key === 'Escape') resetScroll();
-  if (e.key === 'ArrowDown') startScroll('forwards');
-  if (e.key === 'ArrowUp') startScroll('backwards');
+  if (e.key === 'ArrowDown') startScroll(1);
+  if (e.key === 'ArrowUp') startScroll(-1);
   if (e.key === ' ') toggleAnimation();
 }
 
 function init() {
+  el.main = document.querySelector("main");
   window.addEventListener('keyup', controlScroll);
 };
 
